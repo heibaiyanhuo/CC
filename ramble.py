5abc3cb1b5fc06c0ea0606faf821638f2387f60c
Qiyang Gu <puddingtea07@hotmail.com>
2018-03-25 01:14:57 -0400

    dev #19

--- ramble.py
+++ ramble.py
@@ -1,49 +1,73 @@
 
+MAP_SIZE = 50
+VIEW_DEPTH = 4
+
 class Ramble:
 
-    def __init__(self, move, scan, scan_finished=False):
-        # self.scan_finished = scan_finished
-        # if not scan_finished:
-        #     self.map = [[0 for i in range(100)] for j in range(100)]
+    def __init__(self, move, scan):
+        self.map = [['0' for i in range(MAP_SIZE)] for j in range(MAP_SIZE)]
+        self.enemies = [[0 for i in range(MAP_SIZE)] for j in range(MAP_SIZE)]
         self.move = move
         self.scan = scan
+        self.scan_count = 0
         self.curr_direction = 'west'
+        self.need_check = True
+
         self.next_scan()
 
     def next_move(self, scan_result):
-        curr_x, curr_y = -1, -1
+        self_x, self_y = -1, -1
         for coord, obj_data_list in scan_result:
             x, y = coord
-            if len(obj_data_list) > 1:
-                for obj_data in obj_data_list:
-                    d = dict(obj_data)
-                    if d['type'] == 'object' and d['identifier'] == 'game_object_1':
-                        curr_x, curr_y = x, y
-                        break
+            for obj_data in obj_data_list:
+                d = dict(obj_data)
+                if d['type'] == 'terrain':
+                    terrain = d['identifier']
+                    if self.map[x][y] == '0':
+                        self.map[x][y] = '=' if terrain == 'water' else '#'
+                        self.scan_count += 1
+                        if self.scan_count == MAP_SIZE * MAP_SIZE:
+                            self.store_map()
+                elif d['type'] == 'object':
+                    if d['identifier'] == 'game_object_1':
+                        self_x, self_y = x, y
+                    else:
+                        self.enemies[x][y] = 1
+
         if self.curr_direction == 'west':
-            if curr_x != 0:
-                self.move('west')
+            if self_x - VIEW_DEPTH > 0:
+                if self.enemies[self_x - 1][self_y] != 1:
+                    self.move('west')
             else:
-                self.curr_direction = 'south'
-                self.move('south')
+                pass
+                # self.curr_direction = 'south'
+                # self.move('south')
         elif self.curr_direction == 'south':
-            if curr_y != 0:
+            if self_y != 0:
                 self.move('south')
             else:
                 self.curr_direction = 'east'
                 self.move('east')
         elif self.curr_direction == 'east':
-            if curr_x != 49:
+            if self_x != 49:
                 self.move('east')
             else:
                 self.curr_direction = 'north'
                 self.move('north')
         else:
-            if curr_y != 49:
+            if self_y != 49:
                 self.move('north')
             else:
                 self.curr_direction = 'west'
                 self.move('west')
 
     def next_scan(self):
-        self.scan()
+        if self.need_check:
+            self.scan()
+
+    def store_map(self):
+        with open('map.txt', 'w+') as f:
+            for i in range(MAP_SIZE - 1, -1, -1):
+                for j in range(MAP_SIZE):
+                    f.write(self.map[j][i])
+                f.write('\n')
